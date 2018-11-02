1cc80c828551089894d5a21810b86dc7750e3600
Shumaher18 <peshkesv@gmail.com>
2018-11-02 16:11:04 +0300

    02/11/2018 para

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
+var current_elem;
+
 function showHide(a) {
   var elem = document.getElementById(a);
-  console.log(elem.className);
   if (elem.className=="hidden"){
     elem.className="shown";
+    elem.focus();
+    current_elem = elem;
   }
   else {
-    elem.className="hidden";
+    if(event.target.id != current_elem.getAttribute('id')){
+      elem.className="hidden";
+    }
   }
 }
