cbd8651019b5d722460796791bc2f43e1a4ce96b
Shumaher18 <peshkesv@gmail.com>
2019-01-25 15:36:13 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -3,19 +3,14 @@
 <meta charset="utf-8">
 <link rel="stylesheet" href="css/head.css">
 <link rel="stylesheet" href="css/main.css">
-<script type="text/javascript" src="http://code.jquery.com/jquery-1.4.2.min.js"></script>	
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.4.2.min.js"></script>
 </head>
 
 		<body>
 <?php
+
   include 'header.php';
-
-    // if ($_GET["name"]=="moonlite")
-    // { include 'profile.php';}
-    // else {
-    //   include 'content.php';
-    // }
-
+	
 	    switch ($_GET["name"]) {
 	    case moonlite:
 	        include 'profile.php';
@@ -24,7 +19,17 @@
 	        include 'content.php';
 	        break;
 }
+
 ?>
 
     <body>
+			// $c=mysqli_connect('localhost', 'user07', 'Password01','db07');
+			// mysqli_query($c, 'set names utf8');
+			// $qr = mysqli_query($c, 'select Theme from foxs_posts where id=3;');
+			// $qr = mysqli_fetch_assoc($qr);
+			// foreach($qr as $k=>$v){
+			//
+			//  	echo $v;
+			//
+			// };
 <html>
