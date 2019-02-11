111f28405e6f25466d2281fedaadc311c81f8973
Shumaher18 <peshkesv@gmail.com>
2019-02-11 15:56:16 +0300

    Add files via upload

--- content.php
+++ content.php
@@ -8,121 +8,22 @@
 				<div class="date">30.11.2018</div>
 				<div class="post_text">Белки взбить с 1 ст. ложкой лимонного сока, 280 г сахара и 150 г кокоса. Выложить в форме 2 кругов на пергамент и выпекать 50 мин (120°). Один корж промазать расплавленным шоколадом и посыпать кокосом. Пюрировать 125 г ягод, остальные разрезать пополам. Желтки взбить со 150 г сахара и ванилином. В пахту ввести желатин с 2 ст. ложками лимонного сока и соединить с желтками. Дать застыть. Крем разделить пополам. 300 мл взбитых сливок вместе с половиной ягод и пюре подмешать к одной половине крема. 2. Вокруг одного коржа установить бортик. Выложить крем с ягодами, положить второй корж и выложить крем без ягод. Поставить на 3 ч в холодильник. Остатки сливок взбить с закрепителем. Торт смазать и украсить ягодами. А далее.. </div>
 			</div>
-			<div class="posts">
-				<div class="section"></div>
-				<div class="article"></div>
-				<div class="autor"></div>
-				<div class="date"></div>
-				<div class="post_text"></div>
-			</div>
-			<div class="posts">
-				<div class="section"></div>
-				<div class="article"></div>
-				<div class="autor"></div>
-				<div class="date"></div>
-				<div class="post_text"></div>
-			</div>
-			<div class="posts">
-				<div class="section"></div>
-				<div class="article"></div>
-				<div class="autor"></div>
-				<div class="date"></div>
-				<div class="post_text"></div>
-			</div>
-			<div class="posts">
-				<div class="section"></div>
-				<div class="article"></div>
-				<div class="autor"></div>
-				<div class="date"></div>
-				<div class="post_text"></div>
-			</div>
-			<div class="posts">
-				<div class="section"></div>
-				<div class="article"></div>
-				<div class="autor"></div>
-				<div class="date"></div>
-				<div class="post_text"></div>
-			</div>
-			<div class="posts">
-				<div class="section"></div>
-				<div class="article"></div>
-				<div class="autor"></div>
-				<div class="date"></div>
-				<div class="post_text"></div>
-			</div>
-			<div class="posts">
-				<div class="section"></div>
-				<div class="article"></div>
-				<div class="autor"></div>
-				<div class="date"></div>
-				<div class="post_text"></div>
-			</div>
-			<div class="posts">
-				<div class="section"></div>
-				<div class="article"></div>
-				<div class="autor"></div>
-				<div class="date"></div>
-				<div class="post_text"></div>
-			</div>
-			<div class="posts">
-				<div class="section"></div>
-				<div class="article"></div>
-				<div class="autor"></div>
-				<div class="date"></div>
-				<div class="post_text"></div>
-			</div>
-			<div class="posts">
-				<div class="section"></div>
-				<div class="article"></div>
-				<div class="autor"></div>
-				<div class="date"></div>
-				<div class="post_text"></div>
-			</div>
-			<div class="posts">
-				<div class="section"></div>
-				<div class="article"></div>
-				<div class="autor"></div>
-				<div class="date"></div>
-				<div class="post_text"></div>
-			</div>
-			<div class="posts">
-				<div class="section"></div>
-				<div class="article"></div>
-				<div class="autor"></div>
-				<div class="date"></div>
-				<div class="post_text"></div>
-			</div>
-			<div class="posts">
-				<div class="section"></div>
-				<div class="article"></div>
-				<div class="autor"></div>
-				<div class="date"></div>
-				<div class="post_text"></div>
-			</div>
-			<div class="posts">
-				<div class="section"></div>
-				<div class="article"></div>
-				<div class="autor"></div>
-				<div class="date"></div>
-				<div class="post_text"></div>
-			</div>
-			<div class="posts">
-				<div class="section"></div>
-				<div class="article"></div>
-				<div class="autor"></div>
-				<div class="date"></div>
-				<div class="post_text"></div>
-			</div>
-			<div class="posts">
-				<div class="section"></div>
-				<div class="article"></div>
-				<div class="autor"></div>
-				<div class="date"></div>
-				<div class="post_text"></div>
-			</div>
 
+			<?php
+			$link=mysqli_connect('localhost', 'user07', 'Password01','db07');
+			mysqli_query($link, 'set names utf8');
+			$qr = mysqli_query($link, 'select * from foxs_posts;');
 
-
+  		while ($result = mysqli_fetch_array($qr)) {
+				echo "<div class='posts'>";
+    		echo "<div class='section'>{$result['Label']}</div>";
+				echo "<div class='article'>{$result['Theme']}</div>";
+				echo "<div class='autor'>Posted by {$result['Author']}</div>";
+				echo "<div class='date'>{$result['Date']}</div>";
+				echo "<div class='post_text'>{$result['Content']}</div>";
+				echo "</div>";
+  		}
+			?>
 
 		<!-- <div id="divfilterbutton">
 			<a href="javascript:void(0)" onclick="showHide('filterwindow')">ФИЛЬТР</a>
